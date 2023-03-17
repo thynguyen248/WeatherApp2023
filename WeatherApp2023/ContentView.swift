6f1ecf7f09825586507255ed2f2a3f581ecf1034
Thy Nguyen <nguyenthy2408@gmail.com>
2023-03-17 14:51:52 +0700

    Initial Commit

---
+++ WeatherApp2023/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherApp2023
+//
+//  Created by Thy Nguyen on 3/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherApp2023/WeatherApp2023App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp2023App.swift
+//  WeatherApp2023
+//
+//  Created by Thy Nguyen on 3/17/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
